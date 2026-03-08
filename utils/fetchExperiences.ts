53effc0137049944a5f6ab95b0dfe02ce10ba72d
Sreeja Nukarapu <sreeja.nukarapu2263@gmail.com>
2026-03-08 18:09:17 -0400

    fix fetch error handling

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,14 +1,10 @@
 import { Experience } from "../typings";
-
-export const fetchExperience = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`,
-  );
-
-  const data = await res.json();
-  const experiences: Experience[] = data.experiences;
-
-  // console.log('fetching', experiences);
-
-  return experiences;
+export const fetchExperience = async (): Promise<Experience[]> => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+    const data = await res.json();
+    return data.experiences || [];
+  } catch (error) {
+    return [];
+  }
 };
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,14 +1,10 @@
 import { Project } from "../typings";
-
-export const fetchProjects = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`,
-  );
-
-  const data = await res.json();
-  const projects: Project[] = data.projects;
-
-  // console.log('fetching', projects);
-
-  return projects;
+export const fetchProjects = async (): Promise<Project[]> => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
+    const data = await res.json();
+    return data.projects || [];
+  } catch (error) {
+    return [];
+  }
 };
